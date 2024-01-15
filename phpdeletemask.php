7ba94e4d46c47282f58cb5d6fce77596a2526e6b
stsotnikov <stsotnikov@gmail.com>
2024-01-15 16:22:48 +0800

    создать файл phpdeletemask.php

---
+++ phpdeletemask.php
@@ -0,0 +1,55 @@
+<?php
+$folderName = '/home/u192026/vebauto.ru/www/test/'; // в какой папке ищем
+$fileName   = "test.txt";   // какой файл интересует
+
+$found = search_file( $folderName, $fileName );
+
+print_r( $found ); //все найденные файлы в массиве
+
+/* Вернет:
+Array
+(
+	[0] => /home/example.com/xmlrpc.php
+	[1] => /home/example.com/wp-includes/class-wp-xmlrpc-server.php
+	[2] => /home/example.com/wp-includes/wlwmanifest.xml
+)
+*/
+
+/**
+ * Поиск файла по имени во всех папках и подпапках
+ * @param  string  $folderName - пусть до папки
+ * @param  string  $fileName   - искомый файл
+ * @return array   Массив найденных файлов.
+ */
+function search_file( $folderName, $fileName ){
+	$found = array();
+	$folderName = rtrim( $folderName, '/' );
+
+	$dir = opendir( $folderName ); // открываем текущую папку
+
+	// перебираем папку, пока есть файлы
+	while( ($file = readdir($dir)) !== false ){
+		$file_path = "$folderName/$file";
+
+		if( $file == '.' || $file == '..' ) continue;
+
+		// это файл проверяем имя
+		if( is_file($file_path) ){
+			// если имя файла искомое, то вернем путь до него
+			if( false !== strpos($file, $fileName) ) $found[] = $file_path;
+		}
+		// это папка, то рекурсивно вызываем search_file
+		elseif( is_dir($file_path) ){
+			$res = search_file( $file_path, $fileName );
+			$found = array_merge( $found, $res );
+		
+			unlink( $file_path.'/'.$fileName ); // непосредственно удаление найденного файла
+		}
+
+	}
+
+	closedir($dir); // закрываем папку
+
+	return $found;
+}
+?>
